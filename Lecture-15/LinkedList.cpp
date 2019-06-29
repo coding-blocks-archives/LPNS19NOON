c5bd2efa1257fc2785fd8256bedb8bb27330f017
kartik <kartikmathur@MacBook-Pro.local>
2019-06-29 14:36:43 +0530

    Lecture-15

---
+++ Lecture-15/LinkedList.cpp
@@ -0,0 +1,167 @@
+// LinkedList
+#include <iostream>
+using namespace std;
+
+class node{
+public:
+	int data;
+	node* next;
+	node(int d):next(NULL){
+		data=d;
+	}
+};
+
+void InsertAtFront(node* &head,node* &tail,int data){
+	if(head==NULL){
+		node* n=new node(data);
+		head = tail = n;
+	}
+	else{
+		node* n = new node(data);
+		n->next = head;
+		head = n;
+	}
+}
+
+void InsertAtEnd(node* &head,node* &tail,int data){
+	if(head==NULL){
+		node* n=new node(data);
+		head = tail = n;
+	}
+	else{
+		node* n = new node(data);
+		tail->next = n;
+		tail = n;
+	}	
+}
+
+int length(node* head){
+	int count = 0;
+	while(head){
+		count++;
+		head = head->next;
+	}
+	return count;
+}
+
+
+void InsertAtMid(node* &head,node* &tail,int pos,int data){
+	if(pos==0){
+		InsertAtFront(head,tail,data);
+	}
+	else if(pos>=length(head)){
+		InsertAtEnd(head,tail,data);
+
+	}
+	else{
+		int jump = 1;
+		node* temp = head;
+		while(jump<=pos-1){
+			temp=temp->next;
+			jump++;
+		}
+
+		node* n = new node(data);
+		n->next = temp->next;
+		temp->next = n;
+	}
+}
+
+void Print(node* head){
+	while(head){
+		cout<<head->data<<"-->";
+		head = head->next;
+	}
+	cout<<"NULL"<<endl;
+}
+
+void DeleteAtFront(node* &head,node* &tail){
+	if(head==NULL){
+		return;
+	}
+	else if(head->next==NULL){
+		delete head;
+		head = tail = NULL;
+	}
+	else{
+		node* temp = head;
+		head = head->next;
+		delete temp;
+	}
+}
+
+void DeleteAtEnd(node* &head,node* &tail){
+	if(head==NULL){
+		return;
+	}
+	else if(head->next==NULL){
+		delete head;
+		head = tail = NULL;
+	}
+	else{
+		node* temp = head;
+		while(temp->next!=tail){
+			temp=temp->next;
+		}
+
+		temp->next = NULL;
+		delete tail;
+		tail = temp;
+	}	
+}
+
+void DeleteAtMid(node* &head,node* &tail,int pos){
+	if(pos == 0){
+		DeleteAtFront(head,tail);
+	}
+	else if(pos>=length(head)){
+		return;
+	}
+	else if(pos==length(head)-1){
+		DeleteAtEnd(head,tail);
+	}
+	else{
+		int jump = 1;
+		node* temp = head;
+		while(jump<=pos-1){
+			temp = temp->next;
+			jump++;
+		}
+		node* temp1 = temp->next;
+		temp->next = temp->next->next;
+		delete temp1;
+	}
+
+}
+
+int main(){
+	node* head=NULL;
+	node* tail=NULL;
+
+	InsertAtFront(head,tail,0);
+	InsertAtFront(head,tail,1);
+	InsertAtFront(head,tail,2);
+	InsertAtFront(head,tail,3);
+	InsertAtEnd(head,tail,4);
+	InsertAtEnd(head,tail,5);
+	InsertAtMid(head,tail,100,100);
+
+	Print(head);
+	// DeleteAtFront(head,tail);
+	// DeleteAtFront(head,tail);
+	DeleteAtMid(head,tail,6);
+	// Print(head);
+	// DeleteAtEnd(head,tail);
+	Print(head);
+
+	cout<<length(head)<<endl;
+
+
+
+
+
+
+
+
+	return 0; 
+}
