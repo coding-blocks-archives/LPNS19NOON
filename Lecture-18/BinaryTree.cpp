24c33959b9c8551dba78ef0802769fe9daeac45a
kartik <kartik.mathur10@gmail.com>
2019-07-05 14:38:55 +0530

    Lecture-18

--- Lecture-18/BinaryTree.cpp
+++ Lecture-18/BinaryTree.cpp
@@ -19,18 +19,15 @@ node* CreateTree(){
 	if(data == -1){
 		return NULL;
 	}
-
 	node* root = new node(data);
 	root->left = CreateTree();
 	root->right = CreateTree();
 	return root;
-}
-
+}// 8 10 1 -1 -1 6 4 -1 -1 7 -1 -1 3 -1 14 13 -1 -1 -1
 void PreOrder(node* root){
 	if(root == NULL){
 		return;
 	}
-
 	cout<<root->data<<" ";
 	PreOrder(root->left);
 	PreOrder(root->right);
@@ -55,6 +52,23 @@ void PostOrder(node* root){
 }
 
 
+int CountNodes(node* root){
+	if(root == NULL){
+		return 0;
+	}
+
+	return CountNodes(root->left)+CountNodes(root->right)+1;
+}
+
+int Height(node* root){
+	if(root == NULL){
+		return 0;
+	}
+
+	int left = Height(root->left);
+	int right = Height(root->right);
+	return max(left,right)+1;
+}
 
 int main(){
 	node* root = NULL;
@@ -66,6 +80,8 @@ int main(){
 	cout<<endl;
 	PostOrder(root);
 	cout<<endl;
+	cout<<"Nodes : "<<CountNodes(root)<<endl;
+	cout<<"Height :"<<Height(root)<<endl;
 
 
 
