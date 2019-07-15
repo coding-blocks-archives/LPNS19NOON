d857cbd54ed452e381ce14e655847d363453b8f9
kartik <kartik.mathur10@gmail.com>
2019-07-15 13:30:22 +0530

    Lecture-25

--- LECTURE-25/Generic_AdjList.cpp
+++ LECTURE-25/Generic_AdjList.cpp
@@ -72,9 +72,9 @@ public:
 			}
 
 		}
-		for(auto node:dist){
-			cout<<"Distance of "<<node.first<<" from "<<src<<" is "<<node.second<<endl;
-		}
+		// for(auto node:dist){
+		// 	cout<<"Distance of "<<node.first<<" from "<<src<<" is "<<node.second<<endl;
+		// }
 
 		T temp = des;
 		while(temp!=src){
@@ -91,18 +91,37 @@ public:
 
 int main(){
 	Graph<int> g;
+	int board[50] = {0};
+	board[2] = 13;
+	board[5] = 2;
+	board[9] = 18;
+	board[18] = 11;
+	board[17]=-13;
+	board[20] = -14;
+	board[24] = -8;
+	board[25] = -10;
+	board[32] = -2;
+	board[34] = -22;
 
-	g.addEdge(0,1);
-	g.addEdge(2,1);
-	g.addEdge(2,4);
-	g.addEdge(2,3);
-	g.addEdge(4,3);
-	g.addEdge(5,3);
-	g.addEdge(0,4);
+	for(int u=0;u<=36;u++){
+		for(int dice = 1;dice<=6;dice++){
+			int v = u+dice+board[u+dice];
+			g.addEdge(u,v,false);
+		}
+	}
+
+
+	// g.addEdge(0,1);
+	// g.addEdge(2,1);
+	// g.addEdge(2,4);
+	// g.addEdge(2,3);
+	// g.addEdge(4,3);
+	// g.addEdge(5,3);
+	// g.addEdge(0,4);
 
 
 	// g.BFS(0);
-	cout<<g.SSSP(0,5)<<endl;
+	cout<<g.SSSP(0,36)<<endl;
 
 	// g.addEdge("Putin","Trump",false);
 	// g.addEdge("Putin","Modi",false);
